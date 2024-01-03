050aa14e18a2a3bf2c7cb7384e4a9c68257e6e62
21195021-Yani-RegPagi-TI-Sem5-UTS-PWA <126751588+Andanisam@users.noreply.github.com>
2024-01-03 16:43:08 +0700

    Add files via upload

---
+++ 21195021-Yani-RegPagi-TI-Sem5-UAS-PWA/main.js
@@ -0,0 +1,106 @@
+
+window.onload = () => {
+"use strict";
+
+const notificationButton = document.getElementById("enableNotifications");
+let swRegistration = null;
+const TokenElem = document.getElementById("token");
+const ErrElem = document.getElementById("err");
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+const config = { apiKey: "AIzaSyCSvr5VIgBfWuxrGGlIrTuvUASzJSrt-_I",
+authDomain: "yani-9c2da.firebaseapp.com",
+projectId: "yani-9c2da",
+storageBucket: "yani-9c2da.appspot.com",
+messagingSenderId: "475869188568",
+appId: "1:475869188568:web:29f56210971341b5c7a2ad",
+measurementId: "G-7ZVR6TG08K"
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+initializeApp();
+
+function initializeApp() {
+  if ("serviceWorker" in navigator && "PushManager" in window) {
+    console.log("Service Worker and Push is supported");
+    initializeUi();
+    initializeFCM();
+
+    //Register the service worker
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(swReg => {
+        console.log("Service Worker is registered", swReg);
+        swRegistration = swReg;
+      })
+      .catch(error => {
+        console.error("Service Worker Error", error);
+      });
+    navigator.serviceWorker.ready.then(function(registration) {
+      console.log("A service worker is active:", registration.active);
+
+      // At this point, you can call methods that require an active
+      // service worker, like registration.pushManager.subscribe()
+    });
+  } else {
+    console.warn("Push messaging is not supported");
+    notificationButton.textContent = "Push Not Supported";
+  }
+}
+
+function initializeUi() {
+  notificationButton.addEventListener("click", () => {
+    displayNotification();
+  });
+}
+
+function initializeFCM() {
+  messaging
+    .requestPermission()
+    .then(() => {
+      console.log("Notification permission granted.");
+
+      // get the token in the form of promise
+      return messaging.getToken();
+    })
+    .then(token => {
+      TokenElem.innerHTML = "token is : " + token;
+    })
+    .catch(err => {
+      ErrElem.innerHTML = ErrElem.innerHTML + "; " + err;
+      console.log("Unable to get permission to notify.", err);
+    });
+}
+
+function displayNotification() {
+  if (window.Notification && Notification.permission === "granted") {
+    notification();
+  }
+  // If the user hasn't told if he wants to be notified or not
+  // Note: because of Chrome, we are not sure the permission property
+  // is set, therefore it's unsafe to check for the "default" value.
+  else if (window.Notification && Notification.permission !== "denied") {
+    Notification.requestPermission(status => {
+      if (status === "granted") {
+        notification();
+      } else {
+        alert("You denied or dismissed permissions to notifications.");
+      }
+    });
+  } else {
+    // If the user refuses to get notified
+    alert(
+      "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
+    );
+  }
+}
+
+function notification() {
+  const options = {
+    body: "Selamat Datang",
+    icon: "img/bell.png"
+  };
+  swRegistration.showNotification(" ", options);
+}
+}
